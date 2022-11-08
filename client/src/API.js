9365d7ead395ff3fd8b7e09f1b060da889a76dd0
GiuseppeCaruso <s303335@studenti.polito.it>
2022-11-08 12:17:02 +0100

    Added filtering API (client and server side)

--- client/src/API.js
+++ client/src/API.js
@@ -10,13 +10,28 @@ async function getHikes() {
     }
 }
 
-async function getFilteredHikes() {
-    const response = await fetch(APIURL+'/getFilteredHikes');
-    const hikes = await response.json();
-    if (response.ok) {
-      return hikes.map((r) => ({ HikeId: r.HikeId, MapId: r.MapId, start: r.start, end: r.end, Title: r.Title, Length: r.Length, ExpectedTime: r.ExpectedTime, Ascent: r.Ascent, Difficulty: r.Difficulty, ReferencePoints: r.ReferencePoints, Description: r.Description}) )
-    } else {
-      throw hikes; //which will contain an error if it is the case
+async function getFilteredHikes(ExpectedTime, Ascent, MapId, Length, Difficulty) {
+    try
+        {const response = await fetch(APIURL+'/getFilteredHikes', {
+            method: 'GET',
+            body: JSON.stringify({ 
+                "ExpectedTime": ExpectedTime,
+                "Ascent": Ascent,
+                "MapId": MapId,
+                "Length": Length,
+                "Difficulty": Difficulty,
+            }),
+            headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+        const hikes = await response.json();
+        if (response.ok) {
+        return hikes.map((r) => ({ HikeId: r.HikeId, MapId: r.MapId, start: r.start, end: r.end, Title: r.Title, Length: r.Length, ExpectedTime: r.ExpectedTime, Ascent: r.Ascent, Difficulty: r.Difficulty, ReferencePoints: r.ReferencePoints, Description: r.Description}) )
+        } else {
+        throw hikes; //which will contain an error if it is the case
+    }} catch (ex) {
+        throw ex;
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan'); // logging middleware
 const {check, validationResult} = require('express-validator'); // validation middleware
+const cors = require('cors');
 const hikes=require('./modules/Hikes.js');
+
 // init express
 const app = new express();
 const port = 3001;
+
+//init middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+app.use(cors(corsOptions))
 
 
 //get hikes full list
@@ -20,17 +32,53 @@ app.get('/getHikes', (req, res) => {
   });
 
 //get the filtered hikes
-app.get('/getFilteredHikes', (req, res) => {
+app.get('/getFilteredHikes', async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({error: 'cannot process request'});
     }
 
     let filters = req.body;
-    hikes.getFilteredHikes(filters)
-      .then(list => res.json(list))
-      .catch(() => res.status(500).end());
+
+    try {
+        let list = await hikes.getHikes();
+        let returned = filtering(filters, list);
+        res.status(200).json(returned);
+      } catch(err) {
+        res.status(503).json({error: `Error`});
+      }
   });
+
+  //filtering function
+let filtering = (filters, list) => {
+    let vec = [];
+    let flag = true;
+    list.forEach(l => {
+            if(typeof filters.Difficulty !== 'undefined')
+            {
+                if(l.Difficulty !== filters.Difficulty){return;}
+                
+            }
+            if(typeof filters.MapId !== 'undefined')
+            {
+                if(l.MapId !== filters.MapId){return;}
+            }
+            if(typeof filters.Ascent !== 'undefined')
+            {
+                if(l.Ascent !== filters.Ascent){return;}
+            }
+            if(typeof filters.ExpectedTime !== 'undefined')
+            {
+                if(l.ExpectedTime !== filters.ExpectedTime){return;}
+            }
+            if(typeof filters.Length !== 'undefined')
+            {
+                if(l.Length !== filters.Length){return;}
+            }
+            vec.push(l);
+        });
+    return vec;
+}
 
 // activate the server
 app.listen(port, () => {
