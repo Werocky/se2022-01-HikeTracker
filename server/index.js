daca87561b6bbcd7e547ee1edeec50c8e2f6e430
GiuseppeCaruso <s303335@studenti.polito.it>
2022-11-08 10:57:03 +0100

    Added API to retrieve the list of hikes. Filtering still not implemented

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const express = require('express');
-
+const {check, validationResult} = require('express-validator'); // validation middleware
+const hikes=require('./modules/Hikes.js');
 // init express
 const app = new express();
 const port = 3001;
+
+
+//get hikes full list
+app.get('/getHikes', (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({error: 'cannot process request'});
+    }
+    hikes.getHikes()
+      .then(list => res.json(list))
+      .catch(() => res.status(500).end());
+  });
 
 // activate the server
 app.listen(port, () => {
--- server/modules/Hikes.js
+++ server/modules/Hikes.js
@@ -2,8 +2,23 @@
 
 const db = require('./DB').db;
 
-class Hike{
-    /*constructor(){
+/*class Hike{
+    constructor(){
 
-    }*/
-}
+    }
+}*/
+//get hikes
+exports.getHikes = () => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM Hikes';
+      db.all(sql, [], (err, rows) => {
+        if (err) {
+          reject(err);
+          
+        }
+        const hikes = rows.map((r) => ({ HikeId: r.HikeId, MapId: r.MapId, start: r.start, end: r.end, Title: r.Title, Length: r.Length, ExpectedTime: r.ExpectedTime, Ascent: r.Ascent, Difficulty: r.Difficulty, ReferencePoints: r.ReferencePoints, Description: r.Description}));
+        resolve(hikes);
+      });
+    });
+  };
+
