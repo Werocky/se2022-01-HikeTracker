5aba4d92d772fa9e7e1cef98b4dfbcc221a5b756
GiuseppeCaruso <s303335@studenti.polito.it>
2022-11-08 10:27:01 +0100

    Added template

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route,Navigate, useNavigate } from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Router>
+      <App2 />
+    </Router>
+    );
+}
+
+function App2(){
+
+return (
+  <>
+  <Container>
+    </Container>
+    
+  <Routes>
+    </Routes>
+      </>
+ 
+);
 }
 
 export default App;
