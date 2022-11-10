64a944cc23c6b521f97048ded6e42137f1b76b5a
DavideBrl <s259689@studenti.polito.it>
2022-11-10 09:10:14 +0100

    function populateTables with some data for the db

--- server/modules/DB.js
+++ server/modules/DB.js
@@ -12,10 +12,12 @@ class DatabaseConnection {
         await this.createTableUsers();
         await this.createTableHikeLocations();
 
+        await this.populateTables();
+
     }
 
     //example:
-   static createTableHikes() {
+    static createTableHikes() {
         return new Promise(async (resolve, reject) => {
             const sql = "CREATE TABLE IF NOT EXISTS Hikes (HikeID TEXT PRIMARY KEY UNIQUE NOT NULL,Title TEXT, Length FLOAT,  ExpectedTime FLOAT, Ascent FLOAT, Difficulty TEXT, Start TEXT, End TEXT, Description TEXT);";
             this.db.run(sql, [], function (err) {
@@ -41,7 +43,7 @@ class DatabaseConnection {
         });
     }
 
-    static createTableUsers(){
+    static createTableUsers() {
         return new Promise(async (resolve, reject) => {
             const sql = "CREATE TABLE IF NOT EXISTS Users (Id INTEGER PRIMARY KEY UNIQUE NOT NULL, Hash TEXT NOT NULL, salt TEXT NOT NULL,  email TEXT NOT NULL, role TEXT NOT NULL);";
             this.db.run(sql, [], function (err) {
@@ -54,16 +56,64 @@ class DatabaseConnection {
         });
     }
 
-    static async deleteDB(){
+    static async deleteDB() {
         fs.rm('./HikeTracker.db', (err) => {
             if (err) {
                 throw err;
             }
-        
+
             console.log("Delete File successfully.");
         })
     }
-    
+
+    static async populateTables() {
+        await this.populateHikes();
+        await this.populateHikeLocations();
+        await this.populateUsers();
+    }
+
+    static populateHikes() {
+        return new Promise(async (resolve, reject) => {
+            const sql = "INSERT INTO Hikes(HikeID, Title, Length,  ExpectedTime, Ascent, Difficulty, Start, End, Description) VALUES "+
+            "('h1', 'Hike to hut Duca degli Abruzzi', 5.26, 200, 794, 'H', 'id_start1', 'id_end1', 'Classico e frequentato percorso da Cervinia al Rifugio all’Orionde Duca degli Abruzzi (2.805 m.).');";
+            this.db.run(sql, [], function (err) {
+                if (err)
+                    reject(err);
+                else {
+                    resolve('Tables created');
+                }
+            });
+        });
+    }
+
+    static populateHikeLocations() {
+        return new Promise(async (resolve, reject) => {
+            const sql = "INSERT INTO HikeLocations(HikeID, Province, City) VALUES "+
+            "('h1', 'Aosta', 'Cervinia');";
+            this.db.run(sql, [], function (err) {
+                if (err)
+                    reject(err);
+                else {
+                    resolve('Tables created');
+                }
+            });
+        });
+    }
+
+    static populateUsers() {
+        return new Promise(async (resolve, reject) => {
+            const sql = "INSERT INTO Users(Id, Hash, salt, role) VALUES "+
+            "('b@polito.it','1a42b2b340fb544339c01cf46a523f08abdf2f214b43058e163087a4ecd8dfbe',1234, 'h');";
+            this.db.run(sql, [], function (err) {
+                if (err)
+                    reject(err);
+                else {
+                    resolve('Tables created');
+                }
+            });
+        });
+    }
+
 }
 
 module.exports = DatabaseConnection;
