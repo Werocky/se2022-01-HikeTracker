d08270878bd74d338152ac0c3ea412363f783f04
guoyadan <s267337@studenti.polito.it>
2022-12-04 19:51:10 +0100

    layout for hut

--- client/src/components/forms/AddHutForm.js
+++ client/src/components/forms/AddHutForm.js
@@ -17,31 +17,48 @@ import API from "../../API.js";
 import { useNavigate } from "react-router-dom";
 
 const Container = tw.div`relative`;
+const Content = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
+
 const TwoColumn = tw.div`flex flex-col md:flex-row justify-between max-w-screen-xl mx-auto py-20 md:py-24`;
 const Column = tw.div`w-full max-w-md mx-auto md:max-w-none md:mx-0`;
-const ImageColumn = tw(Column)`md:w-5/12 flex-shrink-0 h-80 md:h-auto`;
 const TextColumn = styled(Column)(props => [
-  tw`md:w-7/12 mt-16 md:mt-0`,
+  tw`md:w-6/12 mt-16 md:mt-0`,
   props.textOnLeft ? tw`md:mr-12 lg:mr-16 md:order-first` : tw`md:ml-12 lg:ml-16 md:order-last`
 ]);
 
-const Image = styled.div(props => [
-  `background-image: url("${props.imageSrc}");`,
-  tw`rounded bg-contain bg-no-repeat bg-center h-full`,
-]);
-const TextContent = tw.div`lg:py-8 text-center md:text-left`;
 
-const Subheading = tw(SubheadingBase)`text-center md:text-left`;
-const Heading = tw(SectionHeading)`mt-4 font-black text-left text-3xl sm:text-4xl lg:text-5xl text-center md:text-left leading-tight`;
-const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100`
-const ImageMapColumn = tw(Column)`md:w-5/12 flex-shrink-0 h-80 md:h-auto`;
+const TextContent = tw.div`lg:py-8 text-center md:text-left`;
+const Heading = tw(SectionHeading)`mt-4  font-black text-left text-3xl sm:text-4xl lg:text-5xl text-center md:text-left leading-tight`;
+const Description = tw.p`mt-4 mb-12 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-gray-300`
+const ImageMapColumn = tw(Column)`md:w-6/12 flex-shrink-0 md:h-auto`;
 const Form = tw.form`mt-8 md:mt-10 text-sm flex flex-col max-w-sm mx-auto md:mx-0`
 const Input = tw.input`mt-6 first:mt-0 border-b-2 py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500`
-const Textarea = styled(Input).attrs({as: "textarea"})`
-  ${tw`h-24`}
-`
+const InputOption = tw.input`mt-6 first:mt-0 border-b-2 py-3 focus:outline-none font-medium transition duration-300 hocus:border-gray-300 text-gray-700 `
 
-const SubmitButton = tw(PrimaryButtonBase)`inline-block mt-8`
+const FormContainer = styled.div`
+  ${tw`p-10 sm:p-12 md:p-16 bg-primary-500 text-gray-100 rounded-lg relative`}
+  form {
+    ${tw`mt-4`}
+  }
+  h2 {
+    ${tw`text-3xl sm:text-4xl font-bold`}
+  }
+  input,textarea {
+    ${tw`w-full bg-transparent text-gray-100 text-base font-medium tracking-wide border-b-2 py-2 text-gray-100 hocus:border-pink-400 focus:outline-none transition duration-200`};
+
+    ::placeholder {
+      ${tw`text-gray-400`}
+    }
+  }
+`;
+const InputContainer = tw.div`relative py-5 mt-6`;
+const Label = tw.label`absolute top-0 left-0 tracking-wide font-semibold text-base`;
+const Recap = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-gray-200`
+
+
+const Textarea = tw.textarea`h-24 sm:h-full resize-none`;
+const SubmitButton = tw.button`w-full  mt-6 py-3 bg-gray-100 text-primary-500 rounded-full font-bold tracking-wide shadow-lg uppercase text-sm transition duration-300 transform focus:outline-none focus:shadow-outline hover:bg-gray-300 hover:text-primary-700 hocus:-translate-y-px hocus:shadow-xl`;
+
 const B=tw(PrimaryButtonBase)`inline-block mt-8`
 
 function AddHutForm(props){
@@ -60,7 +77,7 @@ function AddHutForm(props){
   const submitButtonText = "Confirm";
   const formAction = "#";
   const formMethod = "get";
-  const textOnLeft = true;
+  const textOnLeft = false;
 
 // The textOnLeft boolean prop can be used to display either the text on left or right side of the image.
 
@@ -204,28 +221,30 @@ const ClickPick = () => {
   }
   return (
     <AnimationRevealPage>
-    <Header logout={props.logout}/>
-    <Container>
+      <Header logout={props.logout}/>
+      <Container>
+        <Content>
+          <FormContainer>
     <TwoColumn>
     {
-      geoOk &&      <TextContent>
-      {/*{subheading && <Subheading>{subheading}</Subheading>}*/}
-      <Heading>Recap</Heading>
-      {description && <Description>recap of your hut</Description>}
-      <label>Name:{name}</label><br/>
-      <label>Elevation:{elevation} mt</label><br/>
-      <label>City:{city}  &nbsp;</label>
-      <label>Province:{province}  </label><br/>
-      <label>Region:{region} &nbsp; </label>
-      <label>Country:{country}</label><br/>
-      { descr.trim().length!=0 && <><label>description:{descr}</label><br/></>}
-      { phone!=undefined && <><label>phone:{phone}</label><br/></>}
-      {whenOpen!="" && <><label>When open:{whenOpen}</label><br/></>}
-      {avgPrice!=undefined && <><label>Average price:{avgPrice}</label><br/></>}
-      {beds!=undefined && <><label>number of beds:{beds}</label><br/></>}
-      {email.trim().length!=0 && <><label>email:{email}</label><br/></>}
-      {website.trim().length!=0 && <><label>website:{website}</label></>}
+      geoOk &&     <TextColumn textOnLeft={textOnLeft}>
+          <TextContent>
+      <Heading>Recap of your hut</Heading>
+      <Recap>Name : {name}</Recap>
+      <Recap>Elevation : {elevation} mt</Recap>
+      <Recap>City : {city}  &nbsp;</Recap>
+      <Recap>Province : {province}  </Recap>
+      <Recap>Region : {region} &nbsp; </Recap>
+      <Recap>Country : {country}</Recap>
+      {phone!=undefined && <Recap>Phone : {phone}</Recap>}
+      {whenOpen!="" && <Recap>Opening Session : {whenOpen}</Recap>}
+      {avgPrice!=undefined && <Recap>Average price : {avgPrice}</Recap>}
+      {beds!=undefined && <Recap>Number of beds : {beds}</Recap>}
+      {email.trim().length!=0 && <Recap>Email : {email}</Recap>}
+      {website.trim().length!=0 && <Recap>Website : {website}</Recap>}
+      {descr.trim().length!=0 && <Recap>Description : {descr}</Recap>}
       </TextContent>
+        </TextColumn>
     }
   
     {!geoOk &&
@@ -235,7 +254,10 @@ const ClickPick = () => {
                 <Description>Click on the map and insert the type, to add new Reference Point</Description>
 
                 <MapContainer
-                  center={{ lat: 45.063128, lng: 7.661272 }} zoom={8} scrollWheelZoom style={{ height: 420 + "px", width: "100%", }}>
+                  center={{ lat: 45.063128, lng: 7.661272 }}
+                  zoom={8}
+                  scrollWheelZoom
+                  style={{ height: 420 + "px", width: "100%", }}>
                   <ClickPick />
                   <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                 </MapContainer>
@@ -251,34 +273,70 @@ const ClickPick = () => {
       !geoOk &&
    
     <Form onSubmit={submitGeoForm}>
-      <Input type="text" name="name" value={name} placeholder="name" onChange={ev => setName(ev.target.value)}/>
-      <Input type="number" step="1" name="elevation" value={elevation} min={0} placeholder="elevation" onChange={ev=>setElevation(ev.target.value)}  />
-      <Input type="text" name="city"  value={city} placeholder="select a point on the map to select the city"  onChange={ev => setCity(ev.target.value)} />
-      <Input type="text" name="province" value={province} placeholder="select a point on the map to select the province"  onChange={ev => setProvince(ev.target.value)}/>
-      <Input type="text" name="region" value={region} placeholder="select a point on the map to select the region"  onChange={ev => setRegion(ev.target.value)} />
-      <Input type="text" name="country" value={country} placeholder="select a point on the map to select the country" onChange={ev => setCountry(ev.target.value)} />
-     
+      <InputContainer>
+        <Label htmlFor="name-input">Title</Label>
+        <Input id="name-input" type="text" name="name" value={name} placeholder="name" onChange={ev => setName(ev.target.value)}/>
+      </InputContainer>
+      <InputContainer>
+        <Label htmlFor="elevation-input">Elevation</Label>
+        <Input id="elevation-input" type="number" step="1" name="elevation" value={elevation} min={0} placeholder="elevation" onChange={ev=>setElevation(ev.target.value)}  />
+      </InputContainer>
+      <InputContainer>
+        <Label htmlFor="city-input">City</Label>
+        <Input id="city-input" type="text" name="city"  value={city} placeholder="select a point on the map to select the city"  onChange={ev => setCity(ev.target.value)} />
+      </InputContainer>
+      <InputContainer>
+        <Label htmlFor="province-input">Province</Label>
+        <Input id="province-input" type="text" name="province" value={province} placeholder="select a point on the map to select the province"  onChange={ev => setProvince(ev.target.value)}/>
+      </InputContainer>
+      <InputContainer>
+        <Label htmlFor="region-input">Region</Label>
+        <Input id="region-input" type="text" name="region" value={region} placeholder="select a point on the map to select the region"  onChange={ev => setRegion(ev.target.value)} />
+      </InputContainer>
+      <InputContainer>
+        <Label htmlFor="country-input">Country</Label>
+        <Input id="country-input" type="text" name="country" value={country} placeholder="select a point on the map to select the country" onChange={ev => setCountry(ev.target.value)} />
+      </InputContainer>
     <SubmitButton type="submit">Next</SubmitButton>
     </Form>
  }
 
  { geoOk && 
   <Form onSubmit={submitForm}>
-  <Textarea name="description" placeholder="Description" value={descr} onChange={ev => setDescr(ev.target.value)}  required={true}/>
-   <br/><br/><PhoneInput placeholder="Enter phone number"value={phone} onChange={setPhone}  required={true}/> 
- 
-  <Input type="number" step="1" name="numberOfBeds" value={beds} min={0} placeholder="insert number of beds" onChange={ev=>setBeds(ev.target.value)}  required={true} />
-  <Input type="whenOpen" defaultValue={'DEFAULT'} as="select" aria-label="select" onChange={ev => setWhenOpen(ev.target.value)} required={true} >
-        <option value='DEFAULT' hidden>Select when it's open</option>
+    <InputContainer>
+      <Label htmlFor="phone-input">Phone Number</Label>
+      <PhoneInput id="phone-input" placeholder="Enter phone number"value={phone} onChange={setPhone}  required={true}/>
+    </InputContainer>
+    <InputContainer>
+      <Label htmlFor="numberOfBeds-input">Number Of Beds</Label>
+      <Input id="numberOfBeds-input" type="number" step="1" name="numberOfBeds" value={beds} min={0} placeholder="insert number of beds" onChange={ev=>setBeds(ev.target.value)}  required={true} />
+    </InputContainer>
+
+      <InputOption id="open-input" type="whenOpen" defaultValue={'DEFAULT'} as="select" aria-label="select" onChange={ev => setWhenOpen(ev.target.value)} required={true} >
+      <option value='DEFAULT' hidden>Select when it's open</option>
         <option value="W">Winter</option>
         <option value="S">Summer</option>
         <option value="WS">Winter & Summer</option>
         <option value="N">Never</option>
-      </Input>
-  <Input type="number" step="0.5" name="avgprice" value={avgPrice} min={0} placeholder="insert average price for a room" onChange={ev=>setAvgPrice(ev.target.value)}  required={true} />
-  <Input type="email"name="email" value={email}  placeholder="insert email of the hut" onChange={ev=>setEmail(ev.target.value)}  required={true} />
-  <Input type="text"name="webSite" value={website}  placeholder="insert website of the hut (optional)" onChange={ev=>setWebsite(ev.target.value)}  required={false} />
-  <B onClick={()=>setGeoOk(false)}>Back</B>
+      </InputOption>
+
+    <InputContainer>
+      <Label htmlFor="avgprice-input">Average Price</Label>
+      <Input id="avgprice-input" type="number" step="0.5" name="avgprice" value={avgPrice} min={0} placeholder="insert average price for a room" onChange={ev=>setAvgPrice(ev.target.value)}  required={true} />
+    </InputContainer>
+    <InputContainer>
+      <Label htmlFor="email-input">Email</Label>
+      <Input id="email-input" type="email"name="email" value={email}  placeholder="insert email of the hut" onChange={ev=>setEmail(ev.target.value)}  required={true} />
+    </InputContainer>
+    <InputContainer>
+      <Label htmlFor="website-input">Website</Label>
+      <Input id="website-input" type="text"name="webSite" value={website}  placeholder="insert website of the hut (optional)" onChange={ev=>setWebsite(ev.target.value)}  required={false} />
+    </InputContainer>
+    <InputContainer>
+      <Label htmlFor="description-input">Description</Label>
+      <Textarea id="description-input" name="description" placeholder="Description" value={descr} onChange={ev => setDescr(ev.target.value)}  required={true}/>
+    </InputContainer>
+    <B onClick={()=>setGeoOk(false)}>Back</B>
   <SubmitButton type="submit">Confirm</SubmitButton>
 </Form>
  }
@@ -286,6 +344,8 @@ const ClickPick = () => {
     </TextContent>
     </TextColumn>
     </TwoColumn>
+          </FormContainer>
+        </Content>
     </Container>
     </AnimationRevealPage>
   );
