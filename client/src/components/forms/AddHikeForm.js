05621280c54e3db8d91654e71737204d08aa44c3
guoyadan <s267337@studenti.polito.it>
2022-12-04 15:30:51 +0100

    layout small change

--- client/src/components/forms/AddHikeForm.js
+++ client/src/components/forms/AddHikeForm.js
@@ -18,7 +18,7 @@ const Content = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
 
 const TwoColumn = tw.div`flex flex-col md:flex-row justify-between max-w-screen-xl mx-auto py-20 md:py-24`;
 const Column = tw.div`w-full max-w-md mx-auto md:max-w-none md:mx-0`;
-const ImageMapColumn = tw(Column)`md:w-5/12 flex-shrink-0 h-80 md:h-auto`;
+const ImageMapColumn = tw(Column)`md:w-5/12 flex-shrink-0 md:h-auto`;
 const TextColumn = styled(Column)(props => [
   tw`md:w-7/12 mt-16 md:mt-0`,
   props.textOnLeft ? tw`md:mr-12 lg:mr-16 md:order-first` : tw`md:ml-12 lg:ml-16 md:order-last`
