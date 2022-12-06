f90c4ef7b52600d6f327175452cc39603760cee3
Magelli99 <s303447@studenti.polito.it>
2022-12-06 10:27:25 +0100

    logo click

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -66,7 +66,7 @@ function Register(props){
         <Content>
           <MainContainer>
             <LogoLink href={logoLinkUrl}>
-              <LogoImage src={logo} />
+              <LogoImage src={logo}onClick={()=>navigate('/')} />
             </LogoLink>
             <MainContent>
               <Heading>{headingText}</Heading>
