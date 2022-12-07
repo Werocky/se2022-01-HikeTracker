0d8aed8f3d011adcf6991b60275ee470bf024f09
FABIO CHAVEZ s303519 <fabio.alfredo.fc.fc@gmail.com>
2022-12-07 02:17:08 +0100

    added Parking Lot tests

--- server/modules/ParkingLot.js
+++ server/modules/ParkingLot.js
@@ -29,8 +29,23 @@ function createParkingLot(ParkingLot){
 
 function updateParkingLot(id, description, free){
     return new Promise(async (resolve, reject) =>{
-        const sql = 'UPDATE ParkingLots SET Description = ?, Free = ? WHERE ParkingID = ?';
-        db.run(sql, [description, free, id], function (err){
+        try{
+            await queryUpdateParkingLotRP(id, description);
+            await queryUpdateParkingLot(id,free);
+            resolve('Entry Updated Correctly')
+        }catch(err){
+            // undo changes if wanted
+            reject(err);
+        }
+        
+       
+    })
+}
+///:private functions to update in 1 call the parking lot
+function queryUpdateParkingLot(id,free){    
+    return new Promise(async(resolve,reject)=>{
+        const sql = 'UPDATE ParkingLots SET Free = ? WHERE ParkingID = ?';
+        db.run(sql, [free, id], function (err){
             if(err)
                 reject(err);
             else
@@ -38,7 +53,18 @@ function updateParkingLot(id, description, free){
         })
     })
 }
-
+function queryUpdateParkingLotRP(id,description){
+    return new Promise(async(resolve,reject)=>{
+        const sql= "UPDATE ReferencePoints SET description = ? WHERE RefPointID =?";
+        db.serialize(function(){
+            db.run(sql,[description,id],(err)=>{
+                if(err)reject(err);
+                else resolve('referencePoints updated');
+            })
+        })
+    })
+}
+///://
 function getParkingLots(){
     return new Promise(async (resolve, reject) =>{
         const sql = 'SELECT * FROM ParkingLots';
@@ -53,8 +79,8 @@ function getParkingLots(){
 
 function getParkingLot(id){
     return new Promise(async (resolve, reject) =>{
-        const sql = 'SELECT * FROM ParkingLots WHERE ParkingId = ?';
-        db.run(sql, [id], function (err, rows) {
+        const sql = 'SELECT * FROM ParkingLots WHERE ParkingID = ?';
+        db.get(sql, [id], function (err, rows) {
             if(err)
                 reject(err);
             else
@@ -63,12 +89,10 @@ function getParkingLot(id){
     })
 }
 
-
-
 function deleteParkingLot(id){
     return  new Promise(async (resolve, reject) =>{
         const sql = 'DELETE FROM ParkingLots WHERE ParkingID = ?';
-        this.db.run(sql, [id], function (err){
+        db.run(sql, [id], function (err){
             if (err)
                 reject(err);
             else
@@ -90,17 +114,26 @@ function emptyParkingLot() {
 
 function getLastParkingID(){
     return new Promise(async (resolve, reject) =>{
-        const sql = "SELECT MAX(ParkingID) FROM ParkingLots";
-        db.run(sql, [], function (err, rows) {
+        const sql = "SELECT MAX(ParkingID) AS PID FROM ParkingLots";
+        db.get(sql, [], function (err, rows) {
             if(err)
                 reject(err);
             else{
                 if(rows === null || rows === undefined)
                     rows = 0;
-                resolve(rows);
+                resolve(rows.PID);
             }
         })
     })
 }
 
-module.exports = {ParkingLot, createParkingLot, updateParkingLot, getParkingLots, getParkingLot, deleteParkingLot, emptyParkingLot, getLastParkingID};
+module.exports = {
+    ParkingLot,
+    createParkingLot,
+    updateParkingLot,
+    getParkingLots,
+    getParkingLot,
+    deleteParkingLot,
+    emptyParkingLot,
+    getLastParkingID
+};
---
+++ server/unit_test/ParkingLot.test.js
@@ -0,0 +1,71 @@
+const  PL = require('../modules/ParkingLot');
+const db = require("../modules/DB");
+const {ParkingLot}= require('../modules/ParkingLot');
+
+beforeAll(async() =>{   
+    await db.createConnection();
+    await PL.emptyParkingLot();
+});
+afterAll(async() =>{   
+    await PL.emptyParkingLot();
+});
+
+const testPL = new ParkingLot(1,"Associated@guide.fake",1,123);
+describe("ParkingLot Tests",()=>{
+    beforeEach(async() =>{   
+        await PL.emptyParkingLot();
+    });
+    afterEach(async() =>{   
+        await PL.emptyParkingLot();
+    });
+    describe("Add/Delete ParkingLot",()=>{
+        it("Add a new ParkingLot",async()=>{
+            await expect(PL.getParkingLots()).resolves.toEqual([]);
+            await expect(PL.createParkingLot(testPL)).resolves.toEqual('Parking lot created');
+            let P = await PL.getParkingLot(1);
+            expect(P).not.toEqual([]);
+            expect(P).toHaveProperty('ParkingID');
+            expect(P.ParkingID).toEqual(1);
+            expect(P).toHaveProperty('AssociatedGuide');
+            expect(P.AssociatedGuide).toEqual("Associated@guide.fake");
+            expect(P).toHaveProperty('NumAuto');
+            expect(P.NumAuto).toEqual(123);
+            expect(P).toHaveProperty('Free');
+            expect(P.Free).toEqual(1);
+
+        })
+        it("delete a parking lot",async()=>{
+            await expect(PL.getParkingLot(1)).resolves.not.toEqual([]);
+            await expect(PL.deleteParkingLot(1)).resolves.toEqual('Entry deleted')
+            await expect(PL.getParkingLots()).resolves.toEqual([]);
+        })
+    })
+    it("Update ParkingLot",async()=>{
+        await PL.createParkingLot(testPL);
+        let P = await PL.getParkingLot(1);
+        expect(P).not.toEqual([]);
+        expect(P).toHaveProperty('ParkingID');
+        expect(P.ParkingID).toEqual(1);
+        expect(P).toHaveProperty('AssociatedGuide');
+        expect(P.AssociatedGuide).toEqual("Associated@guide.fake");
+        expect(P).toHaveProperty('NumAuto');
+        expect(P.NumAuto).toEqual(123);
+        expect(P).toHaveProperty('Free');
+        expect(P.Free).toEqual(1);
+
+       await expect(PL.updateParkingLot(1,'a description',0)).resolves.toEqual('Entry Updated Correctly')
+       P = await PL.getParkingLot(1);
+       expect(P).not.toEqual([]);
+       expect(P).toHaveProperty('ParkingID');
+       expect(P.ParkingID).toEqual(1);
+       expect(P).toHaveProperty('AssociatedGuide');
+       expect(P.AssociatedGuide).toEqual("Associated@guide.fake");
+       expect(P).toHaveProperty('NumAuto');
+       expect(P.NumAuto).toEqual(123);
+       expect(P).toHaveProperty('Free');
+       expect(P.Free).toEqual(0);
+       await expect(PL.getLastParkingID()).resolves.toEqual(1);
+    })
+    
+
+});
