08badbaf36d6f1ed5a034456875942524c3a6eaf
FABIO CHAVEZ s303519 <fabio.alfredo.fc.fc@gmail.com>
2022-12-17 12:15:42 -0400

    added creation of Active hike Points and addition of one

---
+++ server/unit_test/ActivePoints.test.js
@@ -0,0 +1,8 @@
+const db = require("../modules/DB");
+const ActivePoint= require("../modules/ActiveHike");
+
+describe("",()=>{
+    it("",()=>{
+        expect(ActivePoint).not.toEqual(null);
+    })
+});
